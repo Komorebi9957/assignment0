bb1353e5539443f2b1bb16910c87cf33f2a45b90
bowenxu <shenbolics@gmail.com>
2023-09-11 20:57:04 -0700

    initial welcome script

---
+++ main.py
@@ -0,0 +1,7 @@
+def welcome_message():
+    print("Welcome to My Python Program!")
+    print("================================")
+    name = input("What's your name? ")
+    print(f"Hello, {name}! Welcome to our program.")
+
+    welcome_message()
